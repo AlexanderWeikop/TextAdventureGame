62256e41de21545a7334d4f6532b284d766b4ac8
Alexander <gt40f_000@10.50.130.130>
2018-04-09 12:39:31 +0200

    Init fam

---
+++ TextAdventureGame/src/controller/Controller.java
@@ -0,0 +1,14 @@
+
+package controller;
+
+/**
+ *
+ * @author Alexander W
+ */
+public class Controller {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
