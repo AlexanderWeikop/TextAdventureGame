f5cb17e0907a3159858223a51bf672b5289fff8a
Alexander <gt40f_000@10.50.130.130>
2018-04-09 12:28:17 +0200

    Fix

---
+++ TextAdventureGame/TextAdventureGame/src/controller/Controller.java
@@ -0,0 +1,14 @@
+
+package controller;
+
+/**
+ *
+ * @author Alexander Weikop
+ */
+public class Controller {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
