dad75b79c405d4a67582638019ca71a341e9b573
Alexander <gt40f_000@10.50.130.130>
2018-04-09 09:18:34 +0200

    init fam

---
+++ TextAdventureGame/src/textadventuregame/TextAdventureGame.java
@@ -0,0 +1,14 @@
+
+package textadventuregame;
+
+/**
+ *
+ * @author Alexander Weikop
+ */
+public class TextAdventureGame {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
